5f2cccd3763a95c60861b57215eeb5b47427729b
Ethan Schwartzberg <165211862+Schwartz22@users.noreply.github.com>
2024-04-08 19:22:13 -0400

    Create main-script.py

---
+++ main-script.py
@@ -0,0 +1,2 @@
+""" This is where we will have the main script where we implement the script for asking user data, 
+and creating a file for the user that contains the analysis of the input using classes and functions. """
