21d8097bf0a6e56912075ed8d71badd3f4f48d9a
Ethan Schwartzberg <165211862+Schwartz22@users.noreply.github.com>
2024-05-02 15:59:58 -0400

    Update unit-tests.py

--- unit-tests.py
+++ unit-tests.py
@@ -1,7 +1,12 @@
 """This is the file where we will conduct all of the unit tests for the functions"""
 
 # Unit Test functions for insights.py 
+def test_bmi():
+  data = HealthData(steps = 5000, sleep = 7, height = 69, weight = 160)
+  calculated_bmi = 23.6
+  bmi = data.calculateBmi()
 
+  assert calculated_bmi == bmi, f"Test Failed"
 
 
 
